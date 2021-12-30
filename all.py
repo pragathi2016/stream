c00d064a721dbb898247c10a0faa862f529733b7
PirATesofArabian <62105014+PirATesofArabian@users.noreply.github.com>
2021-12-30 15:29:34 +0530

    n

--- all.py
+++ all.py
@@ -75,9 +75,7 @@ def straddle():
 	st.subheader('4th Strategy- Straddle')
 	st.text("BANKNIFTY")
 	st.write(straddle)
-	straddle_pnl=0
 	for i in straddle['pnl']:
-		st.write(i)
 		i=int(i)
 		straddle_pnl+=i
 	st.text(f'Total pnl in 4th startegy is {straddle_pnl}')
